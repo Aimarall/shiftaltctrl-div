26a78901761f140f835e3b27fa11ab48d42eb379
Aimarall <aimaral21072004@gmail.com>
2021-02-15 14:26:19 +0600

    added divs colors

---
+++ scripts.js
@@ -0,0 +1,42 @@
+document.body.addEventListener('mousedown', function(event){
+    let y = event.clientY;
+    let x = event.clientX;
+    let div = document.createElement("div");
+    div.style.width = "20px";
+    div.style.height = "20px";
+    div.style.backgroundColor = "red";
+    div.style.borderRadius = "100%"
+    div.style.position = "absolute";
+    div.style.top = y + "px";
+    div.style.left = x + "px";
+    
+    if (event.which == 2){
+        div.style.backgroundColor = "blue";
+    }
+
+    if (event.which == 3){
+        div.style.backgroundColor = "green";
+    }
+
+    if (event.ctrlKey) {
+        
+        div.style.borderRadius = "0px";
+        div.style.backgroundColor = "yellow";
+    }
+
+    if (event.shiftKey) {
+        div.style.transform = "rotate(45deg)"
+        div.style.borderRadius = "0px";
+        div.style.backgroundColor = "purple";
+    }
+
+    if (event.altKey) {
+        
+        div.style.width = "25px";
+        div.style.height = "40px";
+        div.style.backgroundColor = "gray";
+    }
+    document.body.append(div);
+});
+
+
